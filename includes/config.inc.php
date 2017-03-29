393cde6ec057dcaa8897ad7a21b2da1d9f12d50c
akshitkrnagpal <nagpalkrakshit@gmail.com>
2017-03-29 17:14:27 +0530

    Fixed

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,9 +1,11 @@
 <?php
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "password");
-define("DB_NAME", "qwikcheck");
+$contents = explode(';',getenv("MYSQLCONNSTR_localdb") );
+
+define("DB_HOST", explode('=',$contents[1])[1]);
+define("DB_USER", explode('=',$contents[2])[1]);
+define("DB_PASS", explode('=',$contents[3])[1]);
+define("DB_NAME","qwikcheck");
 
 define("ROOT","http://localhost");
 
