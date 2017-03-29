8e330fc0606e0d43e35eaf4e2ea83d4c12272667
akshitkrnagpal <nagpalkrakshit@gmail.com>
2017-03-29 16:45:01 +0530

    testing config

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
 
-include_once 'includes/UserHandler.inc.php';
-$userHandler = new UserHandler();
-$apikey = $userHandler->generate_api_key(3);
-echo $apikey;
-echo $userHandler->get_user_id($apikey);
+include_once 'includes/DbHandler.inc.php';
+
+$DbHandler= new DbHandler();
+
+
+
 ?>
